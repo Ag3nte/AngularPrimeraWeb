9357789799571d7112f51f3649ffc1a98d956510
Ag3nte <albertoalbertomax@hotmail.com>
2018-12-22 20:18:56 +0100

    # Añadidas nuevas funcionalidades

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from 'src/app/login/login.component';
+import {PeliculasComponent} from 'src/app/peliculas/peliculas.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'peliculas', component: PeliculasComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,9 @@ import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
+import { PeliculasComponent } from './peliculas/peliculas.component';
+
+
 
 @NgModule({
    declarations: [
@@ -14,11 +18,13 @@ import { LoginComponent } from './login/login.component';
       HeaderComponent,
       MainComponent,
       FooterComponent,
-      LoginComponent
+      LoginComponent,
+      PeliculasComponent
    ],
    imports: [
       BrowserModule,
-      AppRoutingModule
+      AppRoutingModule,
+      HttpClientModule
    ],
    providers: [],
    bootstrap: [
